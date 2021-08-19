fef6a3ec597027d6946578df18ba281962c8ac31
unknown <turetcan@gmail.com>
2021-08-19 15:17:55 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="container">
+    <h3>Form Verileriyle Çalışmak</h3>
+    <hr />
+    <div class="row">
+      <div class="col-md-6">
+        <div class="panel panel-warning">
+          <div class="panel-heading">
+            <h4>Başvuru Formu</h4>
+          </div>
+          <div class="panel-body">
+            <form>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label for="email">Kullanıcı Adı</label>
+                    <input
+                      type="text"
+                      id="username"
+                      class="form-control"
+                      v-model="userData.username"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="password">Şifre</label>
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      v-model.lazy.trim="userData.password"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="age">Yaş</label>
+                    <input
+                      type="number"
+                      id="age"
+                      class="form-control"
+                      v-model.number.trim="userData.age"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label for="message">Açıklama</label><br />
+                  <textarea
+                    id="message"
+                    rows="3"
+                    class="form-control"
+                    v-model="userData.message"
+                  ></textarea>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label>
+                      <input
+                        v-model="userData.interests"
+                        type="checkbox"
+                        value="Yazılım"
+                      />
+                      Yazılım
+                    </label>
+                    <label>
+                      <input
+                        v-model="userData.interests"
+                        type="checkbox"
+                        value="Donanım"
+                      />
+                      Donanım
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 form-group">
+                  <label>
+                    <input
+                      type="radio"
+                      value="Erkek"
+                      v-model="userData.gender"
+                    />
+                    Erkek
+                  </label>
+                  <label>
+                    <input
+                      type="radio"
+                      value="Kadın"
+                      v-model="userData.gender"
+                    />
+                    Kadın
+                  </label>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 from-group">
+                  <label>Şehir</label>
+                  <select v-model="userData.selectedCity" class="form-control">
+                    <option v-for="city in userData.cities" :key="city">
+                      {{ city }}
+                    </option>
+                  </select>
+                </div>
+              </div>
+              <hr />
+              <div class="row">
+                <div class="col-md-12">
+                  <button @click.prevent="submit" class="btn btn-primary">Gönder!</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6" v-if="isSubmitted==true">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h4>Form Verileri</h4>
+          </div>
+          <div class="panel-body">
+            <p>Kullanıcı Adı : {{ userData.username }}</p>
+            <p>Şifre : {{ userData.password }}</p>
+            <p>Yaş : {{ userData.age }}</p>
+            <p style="white-space: pre">Açıklama : {{ userData.message }}</p>
+            <p><strong>İlgi Alanları</strong></p>
+            <ul>
+              <li v-for="item in userData.interests" :key="item">{{ item }}</li>
+            </ul>
+            <p>Cinsiyet : {{ userData.gender }}</p>
+            <p>Şehir : {{ userData.selectedCity }}</p>
+            <p>Toggle :</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userData: {
+        username: "",
+        password: "",
+        age: null,
+        message: "",
+        interests: [],
+        gender: "",
+        cities: ["İstanbul", "Ankara", "Adana", "İzmir", "Bursa"],
+        selectedCity: "",
+      },
+      isSubmitted : false
+    };
+  },
+  methods : {
+    submit(){
+      this.isSubmitted= true;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
